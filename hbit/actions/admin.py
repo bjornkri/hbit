ca8dfa7716f07d06d7c019dd4f66e1c03dce486b
Bjorn Kristinsson <bjorn@bjornkri.com>
2017-11-17 09:51:33 +0100

    Improved action admin

--- hbit/actions/admin.py
+++ hbit/actions/admin.py
@@ -7,7 +7,19 @@ from actions.models import Action
 
 
 class ActionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['get_code', 'get_user', 'timestamp', 'description']
+    search_fields = ['habit__description', 'habit__user__username',
+                     'description', 'habit__code']
+
+    def get_user(self, obj):
+        return obj.habit.user
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'habit__user'
+
+    def get_code(self, obj):
+        return obj.habit.code
+    get_code.short_description = 'Code'
+    get_code.admin_order_field = 'habit__code'
 
 
 admin.site.register(Action, ActionAdmin)
